93d169a780b0ee664086094f1078d3f225cac1dd
GedioneSouza <gedionesouza2003@gmail.com>
2021-07-06 23:47:19 -0300

    Lista 4 - Exercicios Java

---
+++ java/AULAS/src/Lista4/Exercicio1.java
@@ -0,0 +1,37 @@
+package Lista4;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		
+//Faça um programa que crie um vetor por leitura com 5 valores de pontuação de uma
+//Atividade e o escreva em seguida. Encontre após a maior pontuação e a apresente.		
+		
+/* 1. Faça um programa que crie um vetor por leitura com 5 valores de pontuação de uma atividade e o escreva em seguida. Encontre após a maior pontuação e a apresente */
+		
+		
+		Scanner leia = new Scanner(System.in);
+		
+		
+		int pontuacao[] = {10, 7, 2, 1, 5};
+		int resultado=0;
+
+          for(int i =0; i < 5; i++){
+
+          	System.out.println("Digite um numero: ");
+          	pontuacao[i] = leia.nextInt();
+          	
+              if(resultado < pontuacao[i]){
+            	  
+              	  resultado = pontuacao[i];
+              }
+
+            }
+          System.out.println("O maior numero digitado foi: " + resultado);
+
+	      }         		
+	  
+	}
+
