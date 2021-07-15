6fcc7126e0c0d1953e8a07d931452b7270fb6dd0
GedioneSouza <gedionesouza2003@gmail.com>
2021-07-14 22:40:32 -0300

    lista5Exercicios

---
+++ java/AULAS/src/lista5Exercicios/programaAviao.java
@@ -0,0 +1,39 @@
+package lista5Exercicios;
+
+public class programaAviao {
+
+	public static void main(String[] args) {
+
+		/*Crie uma classe avião e apresente os atributos e métodos referentes
+		esta classe, em seguida crie um objeto avião, defina as instancias deste
+		objeto e apresente as informações deste objeto no console.*/
+		
+		Aviao aviao1 = new Aviao(); //instanciando objeto avião 1
+		
+		aviao1.modelo = "BIMOTOR";
+		aviao1.rodas = 3;
+		aviao1.tipo = "PASSAGEIRO 14 LUGARES";
+		aviao1.noAr = false;
+		aviao1.ligado = false;
+		aviao1.velocidade = 0;
+		
+		System.out.println("          SITUAÇÃO DO AVIÃO\n");
+		System.out.println("Velocimetro Atual: " + aviao1.velocidade + " NO AR: " + aviao1.noAr + " LIGADO: "+aviao1.ligado);
+		aviao1.ligar();
+		aviao1.aumentarVelocidade();
+		aviao1.aumentarVelocidade();
+		aviao1.diminuirVelocidade();
+		aviao1.diminuirVelocidade();
+		aviao1.decolando();
+		aviao1.aumentarVelocidade(40);
+		System.out.println("Velocimetro Atual: " + aviao1.velocidade + " NO AR: " + aviao1.noAr + " LIGADO: "+aviao1.ligado);
+		aviao1.diminuirVelocidade(40);
+		System.out.println("Velocimetro Atual: " + aviao1.velocidade + " NO AR: " + aviao1.noAr + " LIGADO: "+aviao1.ligado);
+		aviao1.pousar();
+		System.out.println("Velocimetro Atual: " + aviao1.velocidade + " NO AR: " + aviao1.noAr + " LIGADO: "+aviao1.ligado);
+		aviao1.desliga();
+		System.out.println("Velocimetro Atual: " + aviao1.velocidade + " NO AR: " + aviao1.noAr + " LIGADO: "+aviao1.ligado);
+	}
+
+}
+
